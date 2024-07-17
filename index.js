90d8b899e980d46f987ed55806f07973ce94b8b9
javoh1rqaxramonov <ubotirov22@.com>
2024-07-17 23:32:02 +0500

    finished

---
+++ index.js
@@ -0,0 +1,317 @@
+// 1-masala
+// let a =5;
+// if (a>0){
+//     a++;
+// }
+// console.log(a);
+
+// 2-masala
+// let a = -9 ;
+// if (a>0){
+//         a++;
+//     }
+// else if(a<0){
+//     a-=2;
+// }
+//     console.log(a);
+
+// 3-masala
+// let a = 0;
+// if (a>0) {
+//     a++;
+// } else {
+//     a-=2;
+// }
+
+// console.log(a);
+
+// 4-masala 
+// let a = 2 ;
+// let b =17;
+// let c =11;
+// let counter =0;
+// if (a>0) {
+//     counter++;
+// }
+// if (b>0) {
+//     counter++;
+// }
+// if (c>0) {
+//     counter++;
+// }
+// console.log(counter);
+
+// 5-masala 
+// let a = 2 ;
+// let b =17;
+// let c =-11;
+// let counter =0;
+// let sum =0;
+
+// if (a>0) {
+//     counter++;
+// } else{
+//     sum++;
+// }
+// if (b>0) {
+//     counter++;
+// } else{
+//     sum++;
+// }
+// if (c>0) {
+//     counter++;
+// } else{
+//     sum++;
+// }
+// console.log(counter);
+// console.log(sum);
+
+// 6-masala 
+// let a=9;
+// let b=4;
+// let max=0;
+// if (a>b) {
+//     max=a;
+// } else {
+//     max=b;
+// }
+// console.log(max);
+
+// 7-masala 
+// let a=9;
+// let b=4;
+// let min=0;
+// if (a>b) {
+//     min=a;
+// } else {
+//     min=b;
+// }
+// console.log(min);
+
+// 8-masala 
+// let a=9;
+// let b=4;
+// let min=0;
+// let max=0;
+// if (a>b) {
+//     max=a;
+// } else {
+//     max=b;
+// }
+// if (a<b) {
+//     min=a;
+// } else {
+//     min=b;
+// }
+// console.log(max);
+// console.log(min);
+
+// 9-masala
+// let a=10;
+// let b =10000;
+// if (a<b) {
+//     a+=b;
+// }
+// console.log(a);
+
+// 10-masala 
+// let a=12;
+// let b=13;
+// if (a!=b) {
+//     a+=b;
+// }
+
+// console.log(a);
+
+// 11-masala 
+// let a =12;
+// let b =13;
+// let max =0;
+// if (a!=b) {
+//     max=a;
+// }
+// if (a<b) {
+//     max=b;
+// }
+// if(a=b)  {
+//     a=0;
+//     b=0;
+// }
+// console.log(a);
+// console.log(b);
+// console.log(max);
+
+// 12-masala 
+// let a =1;
+// let b =5;
+// let c =3;
+// let max=0;
+// if (a>b || a<b) {
+//     max=a;
+//     max=b
+// } 
+//  else  {
+//     max<c
+//     max=c;
+
+// }
+// console.log(max);
+
+// 13-masala 
+// 14-masala 
+// let a=9;
+// let b=4;
+// let c=5
+// let min=0;
+// let max=0;
+// if (a>b) {
+//     max=a;
+// } else {
+//     min=b;
+// }
+// if (a<b) {
+//     min=a;
+// } else {
+//     max=b;
+// }
+// if (max>c ) {
+//     max=max
+// } else{
+//     min=c;
+// }
+    
+// console.log(min);
+// console.log(max);
+ 
+// 1-masala 
+// let a= 32;
+// if (a>0) {
+//     a=true;
+// }
+// console.log(a);
+
+// 2-masala 
+// let a= 32;
+// if (a/2) {
+//     a=false;
+// } else {
+//     a=true;
+// }
+// console.log(a);
+
+// 3-masala 
+// let a= 32;
+// if (a/2) {
+//     a=true;
+// } else{
+//     a=false;
+// }
+// console.log(a);
+
+// 4-masala
+// let a=3;
+// let b=6;
+// if (a>2 && b<=3) {
+//     a=true;
+//     b=true;
+// } else{
+//     a=false;
+//     b=false;
+// }
+// console.log(a);
+// console.log(b);
+
+// 5-masala 
+// let a=3;
+// let b=6;
+// if (a>0 || b<=-2) {
+//     a=true;
+//     b=true;
+// } else{
+//     a=false;
+//     b=false;
+// }
+// console.log(a);
+// console.log(b);
+
+// 6-masala
+// let a=3;
+// let b=4;
+// let c=5;
+// if (a<=b<=c) {
+//     a =true;
+//     b =true;
+//     c =true;
+// } else {
+//     a=false;
+//     b=false;
+//     c=false;
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// 7-masala 
+// let a=3;
+// let b=4;
+// let c=5;
+// if (a<b && b<c) {
+//     a =true;
+//     b =true;
+//     c =true;
+// } else {
+//     a=false;
+//     b=false;
+//     c=false;
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// 8-masala
+// let a= 32;
+// let b=24;
+// if (a/!2) {
+//     a=false;
+// } else{
+//     a=true;
+// }
+// if (b/!2) {
+//     b=false;
+// } else{
+//     b=true;
+// }
+// console.log(a);
+// console.log(b);
+
+// 9-masala 
+// let a= 32;
+// let b=23;
+// if (a/2) {
+//     a=false;
+// } else{
+//     a=true;
+// }
+// if (b/1 && b/b) {
+//     b=true;
+// } else{
+//     b=false;
+// }
+// console.log(a);
+// console.log(b);
+
+// 10-masala
+// let a= 356;
+// let b=97;
+// if (a/2) {
+//     a=false;
+// } else{
+//     a=true;
+// }
+// if (b/1 && b/b) {
+//     b=true;
+// } else{
+//     b=false;
+// }
+// console.log(a);
+// console.log(b);
